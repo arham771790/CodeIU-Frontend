65602626ec1768d0a7e99865b39b8f2b8ca9cb55
wasif <syedwasifhussain123456@gmail.com>
2026-01-04 22:22:52 +0530

    updated the middleware and create-problem route

---
+++ src/app/(pages)/problems/CreateProblem/page.jsx
@@ -0,0 +1,14 @@
+
+import { CreateProblemForm } from "@/app/components/CreateProblemForm";
+
+
+const AddProblem = () => {
+ 
+  return (
+    <div>
+      <CreateProblemForm />
+    </div>
+  );
+};
+
+export default AddProblem;
