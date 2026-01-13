8925ef9ad6c4581a186be15783eacfb7f5c55979
arham771790 <arhamsheikh6851@gmail.com>
2026-01-14 03:00:04 +0530

    Debug:Fixed that fuckin issue

--- src/app/(pages)/contest/page.jsx
+++ src/app/(pages)/contest/page.jsx
@@ -1,6 +1,8 @@
 import { getContests } from "@/lib/services/contestService";
 import ContestDashboard from "@/app/components/contest/ContestDashboard";
 
+export const dynamic = 'force-dynamic';
+
 export default async function ContestPage() {
   // 1. Fetch on Server (Instant Load + SEO)
   const initialContests = await getContests();
