9f5e198230b253d00c5846138c51453497d8f351
arhamsheikh6851@gmail.com <arhamsheikh6851@gmail.com>
2026-02-23 15:52:43 +0530

    Search Problems Fixed

--- src/app/components/ProblemFilters.jsx
+++ src/app/components/ProblemFilters.jsx
@@ -7,7 +7,7 @@ import { useAuthStore } from "../store/useAuthStore";
 
 export default function ProblemFilters() {
   const searchParams = useSearchParams();
-  const navigate=useRouter();
+  const navigate = useRouter();
   const { authUser } = useAuthStore();
 
 
@@ -15,15 +15,15 @@ export default function ProblemFilters() {
   // e.g. /problems -> /problems?q=two+sum
   const handleSearch = useDebouncedCallback((term) => {
     const params = new URLSearchParams(searchParams);
-    
+
     if (term) {
       params.set("q", term);
     } else {
       params.delete("q");
     }
-    
+
     // replace() updates the URL without reloading the page
-    router.replace(`?${params.toString()}`);
+    navigate.replace(`?${params.toString()}`);
   }, 300); // Wait 300ms after typing stops
 
 
@@ -41,18 +41,18 @@ export default function ProblemFilters() {
           placeholder="Search problems..."
           /* SENIOR FIX: bg-base-200 and border-base-content/20 */
           className="w-full bg-base-200 border border-base-content/20 rounded-full py-2.5 pl-10 pr-4 text-sm text-base-content focus:outline-none focus:border-primary transition-all"
-          defaultValue={searchParams.get("q")?.toString()} 
+          defaultValue={searchParams.get("q")?.toString()}
           onChange={(e) => handleSearch(e.target.value)}
         />
       </div>
 
       <div className="flex items-center gap-3">
         {authUser?.role === "ADMIN" && (
-           <button onClick={handleCreateProblem} className="btn btn-circle btn-ghost border border-base-content/10 bg-base-200 hover:bg-base-300">
-             <Plus className="w-5 h-5 opacity-70" />
-           </button>
+          <button onClick={handleCreateProblem} className="btn btn-circle btn-ghost border border-base-content/10 bg-base-200 hover:bg-base-300">
+            <Plus className="w-5 h-5 opacity-70" />
+          </button>
         )}
-        
+
         <button className="btn btn-circle btn-ghost border border-base-content/10 bg-base-200 hover:bg-base-300">
           <Filter className="w-5 h-5 opacity-70" />
         </button>
